8bc0254d56c72b078b756b1742ab440a387e6b9c
Charan007x <saicharangottipalli@gmail.com>
2026-02-10 11:07:28 +0530

    Initial Boiler Plate

---
+++ backend/src/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import env from 'dotenv';
+import {connectDB} from './config/db.js';
+env.config();
+connectDB();
+const app=express();
+const port =env.PORT || 5000;
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);  
+});
